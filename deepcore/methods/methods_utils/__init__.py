938bf6441595fd01b6a2f713d2b93337bb42b062
Chengcheng-Guo <92306861+Chengcheng-Guo@users.noreply.github.com>
2022-03-31 00:12:00 +0800

    Delete deepcore directory

--- deepcore/methods/methods_utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .euclidean import *
-from .cossim import *
-from .submodular_function import *
-from .submodular_optimizer import *
--- deepcore/nets/__init__.py
+++
@@ -1,9 +0,0 @@
-from .AlexNet import *
-from .LeNet import *
-from .MLP import *
-from .ResNet import *
-from .VGG import *
-from .WideResNet import *
-from .temp import *
-
-model_choices = ["AlexNet",'LeNet',"MLP", "MobileNetV3Large", "MobileNetV3Small", 'ResNet18BN', 'ResNet18', 'ResNet34', 'ResNet50', 'ResNet101', 'ResNet152','VGG11', 'VGG11BN', 'VGG13', 'VGG16', 'VGG19','WRN168', 'WRN2810', 'WRN282', "WRN502", "WRN1012"]
